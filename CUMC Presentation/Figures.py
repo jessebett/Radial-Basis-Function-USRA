c82e06cb56014e0daf52ce92187450bb3da28ac2
Jesse <jessebett@gmail.com>
2014-06-30 17:16:39 -0400

    Python code for presentation.tex figure generation

---
+++ CUMC Presentation/Figures.py
@@ -0,0 +1,81 @@
+
+# The SciPy interpolate library has a Radial Basis Function called Rbf
+from scipy.interpolate import Rbf
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+plt.close("all")
+
+
+def rgb(colour):
+    return tuple(x / 255.0 for x in colour)
+buf = 0.4
+grey = '#181818'
+pink = '#FC0964'
+orange = '#FD971F'
+blue = '#66D9EF'
+red='#D25252'
+green='#7FB347'
+
+
+def jesseaxis(ax):
+    ax.set_axis_bgcolor(grey)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.spines['bottom'].set_color('white')
+    ax.spines['left'].set_color('white')
+    ax.set_xlim(min(x) - buf, max(x) + buf)
+    ax.set_ylim(min(y) - buf, max(y) + buf)
+
+
+####FIGURE 1####
+# Defining the data sites
+fig1 = plt.figure(facecolor=grey)
+ax1 = plt.axes()
+
+x = np.linspace(-6, 6, 20)
+y = (5 * np.sin(x) + x)
+y = y + max(y)
+ax1.scatter(x, y, color=pink, marker='o')
+ax1.scatter(x, [-buf] * np.size(y), color=orange, marker='o')
+ax1.vlines(x, -buf, y, pink, linestyles='dashed', alpha=0.4)
+ax1.annotate('$x_i$', (-0.3, 0.4), size=30, color=orange)
+ax1.annotate('$f_i$', (-0.30, 9), size=30, color=pink)
+ax1.annotate('$s(x)$', (4, 9), size=30, color=blue)
+jesseaxis(ax1)
+# Defining our interpolation function from the data sites
+rbf = Rbf(x, y, epsilon=0.2)
+# Applying the interpolation
+xi = np.linspace(-6, 6, 100)
+yi = rbf(xi)
+plt.plot(xi, yi, color=blue)
+plt.savefig('Images/fig1.png', bbox_inches='tight',
+            pad_inches=0.1, facecolor=grey, dpi=500)
+# plt.show()
+
+
+###INTERP VS APPROX###
+plt.close()
+x = np.array([0.0, 1.0, 2.0, 3.0,  4.0,  5.0])
+y = np.array([0.0, 0.8, 0.9, 0.1, -0.8, -1.0])
+z = np.polyfit(x, y, 3)
+p = np.poly1d(z)
+p30 = np.poly1d(np.polyfit(x, y, 30))
+xp = np.linspace(-2, 6, 100)
+ax2 = plt.axes()
+plt.plot(x, y, 'o', color=pink)
+plt.plot(xp, p(xp), '--', color=green)
+plt.plot(xp, p30(xp), '-', color=orange)
+ax2.annotate('Interpolation', (3.5,0.5), size=20, color=orange)
+ax2.annotate('Approximation', (0,-0.5), size=20, color=green)
+plt.ylim(-2, 2)
+
+jesseaxis(ax2)
+plt.savefig('Images/fig2.png', bbox_inches='tight',
+            pad_inches=0.1, facecolor=grey, dpi=500)
+#plt.show()
