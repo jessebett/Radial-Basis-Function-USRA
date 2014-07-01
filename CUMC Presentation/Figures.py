dba229d16ccb40d330788aae5a9f3f4b93f5a31d
Jesse <jessebett@gmail.com>
2014-07-01 14:05:45 -0400

    Added Figs 3,4. Changed all to functions for faster individual builds

--- CUMC Presentation/Figures.py
+++ CUMC Presentation/Figures.py
@@ -3,7 +3,6 @@
 from scipy.interpolate import Rbf
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
 
 plt.close("all")
 
@@ -15,8 +14,8 @@ grey = '#181818'
 pink = '#FC0964'
 orange = '#FD971F'
 blue = '#66D9EF'
-red='#D25252'
-green='#7FB347'
+red = '#D25252'
+green = '#7FB347'
 
 
 def jesseaxis(ax):
@@ -34,48 +33,90 @@ def jesseaxis(ax):
 
 
 ####FIGURE 1####
-# Defining the data sites
-fig1 = plt.figure(facecolor=grey)
-ax1 = plt.axes()
+def makefig1():
+    # Defining the data sites
+    fig1 = plt.figure(facecolor=grey)
+    ax1 = plt.axes()
 
-x = np.linspace(-6, 6, 20)
-y = (5 * np.sin(x) + x)
-y = y + max(y)
-ax1.scatter(x, y, color=pink, marker='o')
-ax1.scatter(x, [-buf] * np.size(y), color=orange, marker='o')
-ax1.vlines(x, -buf, y, pink, linestyles='dashed', alpha=0.4)
-ax1.annotate('$x_i$', (-0.3, 0.4), size=30, color=orange)
-ax1.annotate('$f_i$', (-0.30, 9), size=30, color=pink)
-ax1.annotate('$s(x)$', (4, 9), size=30, color=blue)
-jesseaxis(ax1)
-# Defining our interpolation function from the data sites
-rbf = Rbf(x, y, epsilon=0.2)
-# Applying the interpolation
-xi = np.linspace(-6, 6, 100)
-yi = rbf(xi)
-plt.plot(xi, yi, color=blue)
-plt.savefig('Images/fig1.png', bbox_inches='tight',
-            pad_inches=0.1, facecolor=grey, dpi=500)
-# plt.show()
+    x = np.linspace(-6, 6, 20)
+    y = (5 * np.sin(x) + x)
+    y = y + max(y)
+    ax1.scatter(x, y, color=pink, marker='o')
+    ax1.scatter(x, [-buf] * np.size(y), color=orange, marker='o')
+    ax1.vlines(x, -buf, y, pink, linestyles='dashed', alpha=0.4)
+    ax1.annotate('$x_i$', (-0.3, 0.4), size=30, color=orange)
+    ax1.annotate('$f_i$', (-0.30, 9), size=30, color=pink)
+    ax1.annotate('$s(x)$', (4, 9), size=30, color=blue)
+    jesseaxis(ax1)
+    # Defining our interpolation function from the data sites
+    rbf = Rbf(x, y, epsilon=0.2)
+    # Applying the interpolation
+    xi = np.linspace(-6, 6, 100)
+    yi = rbf(xi)
+    plt.plot(xi, yi, color=blue)
+    plt.savefig('Images/fig1.png', bbox_inches='tight',
+                pad_inches=0.1, facecolor=grey, dpi=500)
+    # plt.show()
 
 
 ###INTERP VS APPROX###
-plt.close()
-x = np.array([0.0, 1.0, 2.0, 3.0,  4.0,  5.0])
-y = np.array([0.0, 0.8, 0.9, 0.1, -0.8, -1.0])
-z = np.polyfit(x, y, 3)
-p = np.poly1d(z)
-p30 = np.poly1d(np.polyfit(x, y, 30))
-xp = np.linspace(-2, 6, 100)
-ax2 = plt.axes()
-plt.plot(x, y, 'o', color=pink)
-plt.plot(xp, p(xp), '--', color=green)
-plt.plot(xp, p30(xp), '-', color=orange)
-ax2.annotate('Interpolation', (3.5,0.5), size=20, color=orange)
-ax2.annotate('Approximation', (0,-0.5), size=20, color=green)
-plt.ylim(-2, 2)
+def makefig2():
+    plt.close()
+    x = np.array([0.0, 1.5, 2.0, 3.0,  4.0,  4.5])
+    y = np.array([0.0, 0.8, 0.9, 0.1, -0.8, -1.0])
+    z = np.polyfit(x, y, 3)
+    p = np.poly1d(z)
+    p30 = np.poly1d(np.polyfit(x, y, 5))
+    xp = np.linspace(-2, 6, 100)
+    ax2 = plt.axes()
+    plt.plot(x, y, 'o', color=pink)
+    plt.plot(xp, p(xp), '--', color=green)
+    plt.plot(xp, p30(xp), '-', color=orange)
+    ax2.annotate('Interpolation', (3.3, 0.5), size=20, color=orange)
+    ax2.annotate('Approximation', (0, -0.5), size=20, color=green)
+    plt.ylim(-2, 2)
 
-jesseaxis(ax2)
-plt.savefig('Images/fig2.png', bbox_inches='tight',
-            pad_inches=0.1, facecolor=grey, dpi=500)
-#plt.show()
+    jesseaxis(ax2)
+    plt.savefig('Images/fig2.png', bbox_inches='tight',
+                pad_inches=0.1, facecolor=grey, dpi=500)
+    # plt.show()
+
+###POLYNOMIAL INTERP###
+def makefig3():
+    plt.close()
+    x = np.array([0.0, 1.5, 2.0, 3.0,  4.0,  4.5])
+    y = np.array([0.0, 0.8, 0.9, 0.1, -0.8, -1.0])
+    z = np.polyfit(x, y, 3)
+    p = np.poly1d(z)
+    p30 = np.poly1d(np.polyfit(x, y, 5))
+    xp = np.linspace(-2, 6, 100)
+    ax3 = plt.axes()
+    plt.plot(x, y, 'o', color=pink)
+    #plt.plot(xp, p(xp), '--', color=green)
+    plt.plot(xp, p30(xp), '-', color=orange)
+    # ax3.annotate('$s(x)=-0.02988 x^5 + 0.417 x^4 - 2.018 x^3 + 3.694 x^2 - 1.722 x - 5.511e^{-14}$', (-0.2, -1.2), size=13, color=orange)
+    ax3.annotate('$s(x)$', (1.5,0.3), size=30, color=orange)
+    plt.ylim(-2, 2)
+
+    jesseaxis(ax3)
+    plt.savefig('Images/fig3.png', bbox_inches='tight',
+               pad_inches=0.1, facecolor=grey, dpi=500)
+    # plt.show()
+
+###POLYNOMIAL INTERP###
+def makefig4():
+    plt.close()
+    x = np.linspace(-4, 4, 1000)
+    ax = plt.axes()
+    plt.plot(x, np.abs(x), '-', color=green)
+    plt.plot(0,0, 'o', color=pink)
+
+
+    # ax3.annotate('$s(x)=-0.02988 x^5 + 0.417 x^4 - 2.018 x^3 + 3.694 x^2 - 1.722 x - 5.511e^{-14}$', (-0.2, -1.2), size=13, color=orange)
+    ax.annotate('Basic Function', (-1.5,2.5), size=30, color=green)
+
+    jesseaxis(ax)
+    plt.ylim(-1,3)
+    # plt.savefig('Images/fig3.png', bbox_inches='tight',
+    #            pad_inches=0.1, facecolor=grey, dpi=500)
+    plt.show()
