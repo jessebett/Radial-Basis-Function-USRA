435969335bcaa9d108976d88f7057976b98bd64e
Jesse <jessebett@gmail.com>
2014-07-02 00:51:26 -0400

    Added functions for kernel figure generation

--- CUMC Presentation/Figures.py
+++ CUMC Presentation/Figures.py
@@ -18,7 +18,7 @@ red = '#D25252'
 green = '#7FB347'
 
 
-def jesseaxis(ax,x,y):
+def jesseaxis(ax, x, y):
     ax.set_axis_bgcolor(grey)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
@@ -31,11 +31,14 @@ def jesseaxis(ax,x,y):
     ax.set_xlim(min(x) - buf, max(x) + buf)
     ax.set_ylim(min(y) - buf, max(y) + buf)
 
+
 def saveimg(name):
-    plt.savefig('Images/'+name+'.png', bbox_inches='tight',
-               pad_inches=0.1, facecolor=grey, dpi=500)
+    plt.savefig('Images/' + name + '.png', bbox_inches='tight',
+                pad_inches=0.1, facecolor=grey, dpi=500)
 
 ####FIGURE 1####
+
+
 def makefig1():
     # Defining the data sites
     fig1 = plt.figure(facecolor=grey)
@@ -50,7 +53,7 @@ def makefig1():
     ax1.annotate('$x_i$', (-0.3, 0.4), size=30, color=orange)
     ax1.annotate('$f_i$', (-0.30, 9), size=30, color=pink)
     ax1.annotate('$s(x)$', (4, 9), size=30, color=blue)
-    jesseaxis(ax1,x,y)
+    jesseaxis(ax1, x, y)
     # Defining our interpolation function from the data sites
     rbf = Rbf(x, y, epsilon=0.2)
     # Applying the interpolation
@@ -78,7 +81,7 @@ def makefig2():
     ax2.annotate('Approximation', (0, -0.5), size=20, color=green)
     plt.ylim(-2, 2)
 
-    jesseaxis(ax2,x,y)
+    jesseaxis(ax2, x, y)
     saveimg('fig2')
     # plt.show()
 
@@ -101,7 +104,7 @@ def makefig3():
     ax3.annotate('$s(x)$', (1.5, 0.3), size=30, color=orange)
     plt.ylim(-2, 2)
 
-    jesseaxis(ax3,x,y)
+    jesseaxis(ax3, x, y)
     saveimg('fig3')
     # plt.show()
 
@@ -113,16 +116,17 @@ def makefig4():
     x = np.linspace(-4, 4, 1000)
     ax = plt.axes()
     plt.plot(x, np.abs(x), '-', color=green)
-    plt.plot(0, 0, 'o', color=pink)
+    plt.plot(0, 0, 'o', color=orange)
 
     # ax3.annotate('$s(x)=-0.02988 x^5 + 0.417 x^4 - 2.018 x^3 + 3.694 x^2 - 1.722 x - 5.511e^{-14}$', (-0.2, -1.2), size=13, color=orange)
     ax.annotate('Basic Function', (-1.9, 2.59), size=30, color=green)
-    ax.annotate('Center', (-0.9, -0.5), size=30, color=pink)
+    ax.annotate('Center', (-0.9, -0.5), size=30, color=orange)
 
-    jesseaxis(ax,x,np.abs(x))
+    jesseaxis(ax, x, np.abs(x))
     plt.ylim(-1, 3)
     saveimg('fig4')
     plt.show()
+
 
 def makefig5():
     # Defining the data sites
@@ -137,11 +141,11 @@ def makefig5():
     ax1.vlines(x, -buf, y, pink, linestyles='dashed', alpha=0.4)
     ax1.annotate('$x_i$', (-0.3, 0.4), size=30, color=orange)
     ax1.annotate('$f_i$', (-0.30, 7.5), size=30, color=pink)
-    ax1.annotate('$\psi_i$', (4,1), size=30, color=green)
+    ax1.annotate('$\psi_i$', (4, 1), size=30, color=green)
 
-    plt.plot(x, np.abs(x-x[10])-buf, '-', color=green)
+    plt.plot(x, np.abs(x - x[10]) - buf, '-', color=green)
     # ax1.annotate('$s(x)$', (4, 9), size=30, color=blue)
-    jesseaxis(ax1,x,y)
+    jesseaxis(ax1, x, y)
     # Defining our interpolation function from the data sites
     # rbf = Rbf(x, y, epsilon=0.2)
     # Applying the interpolation
@@ -150,4 +154,125 @@ def makefig5():
     # plt.plot(xi, yi, color=blue)
     saveimg('fig5')
     plt.show()
-makefig5()
+
+
+def makefig6():
+    plt.close()
+    x = np.linspace(-4, 4, 1000)
+    ax = plt.axes()
+    plt.plot(x, np.abs(x), '-', color=green)
+    plt.plot(0, 0, 'o', color=orange)
+
+    ax.annotate('Basic Function', (-1.9, 2.59), size=30, color=green)
+    ax.annotate('$x_i$', (-0.2, -0.4), size=30, color=orange)
+
+    jesseaxis(ax, x, np.abs(x))
+    plt.ylim(-1, 3)
+    saveimg('fig6')
+    plt.show()
+
+
+def makefig7():
+    plt.close()
+    x = np.linspace(-4, 4, 1000)
+    ax = plt.axes()
+    plt.plot(x, np.sqrt(x ** 2 + 0.4 ** 2), '-', color=green)
+    plt.plot(0, 0, 'o', color=orange)
+    plt.vlines(0, 0.4, 0, pink, linestyles='dashed', alpha=0.7)
+
+    ax.annotate('Multiquadrc Kernel', (-2.4, 2.59), size=30, color=green)
+    ax.annotate('$x_i$', (-0.2, -0.4), size=30, color=orange)
+    ax.annotate('$c$', (0.2, 0.1), size=30, color=pink)
+    jesseaxis(ax, x, x)
+    plt.ylim(-1, 3)
+    saveimg('fig7')
+    plt.show()
+
+
+def makefig8():
+    # Defining the data sites
+    fig1 = plt.figure(facecolor=grey)
+    ax1 = plt.axes()
+
+    x = np.linspace(-6, 6, 20)
+    xi = np.linspace(-6, 6, 200)
+    y = (5 * np.sin(x) + x)
+    y = y + max(y)
+    ax1.scatter(x, y, color=pink, marker='o')
+    ax1.scatter(x, [-buf] * np.size(y), color=orange, marker='o')
+    ax1.vlines(x, -buf, y, pink, linestyles='dashed', alpha=0.4)
+    ax1.annotate('$x_i$', (-0.3, 0.4), size=30, color=orange)
+    ax1.annotate('$f_i$', (-0.30, 7.5), size=30, color=pink)
+    ax1.annotate('$\psi_i$', (4, 1), size=30, color=green)
+
+    plt.plot(xi, np.sqrt((xi - x[10]) ** 2 + 0.4 ** 2) - buf, '-', color=green)
+    # ax1.annotate('$s(x)$', (4, 9), size=30, color=blue)
+    jesseaxis(ax1, x, y)
+    # Defining our interpolation function from the data sites
+    # rbf = Rbf(x, y, epsilon=0.2)
+    # Applying the interpolation
+    # xi = np.linspace(-6, 6, 100)
+    # yi = rbf(xi)
+    # plt.plot(xi, yi, color=blue)
+    saveimg('fig8')
+    plt.show()
+
+
+def makeMultiQuadric():
+    plt.close()
+    x = np.linspace(-4, 4, 1000)
+    ax = plt.axes()
+    plt.plot(x, np.sqrt(1 + (np.abs(x) * (1 / 0.8)) ** 2), '-', color=green)
+    plt.plot(0, 0, 'o', color=orange)
+    jesseaxis(ax, x, x)
+    ax.axis('off')
+    plt.ylim(-0.2, 3)
+    saveimg('multiquadric')
+    # plt.show()
+
+
+def makeGaussian():
+    plt.close()
+    x = np.linspace(-4, 4, 1000)
+    ax = plt.axes()
+    plt.plot(x, np.exp(-(np.abs(x) * 0.4) ** 2), '-', color=green)
+    plt.plot(0, 0, 'o', color=orange)
+    jesseaxis(ax, x, x)
+    ax.axis('off')
+    plt.ylim(-0.2, 1.2)
+    saveimg('gaussian')
+    # plt.show()
+
+
+def makeInverseQuadratic():
+    plt.close()
+    x = np.linspace(-4, 4, 1000)
+    ax = plt.axes()
+    plt.plot(x, (1 + (np.abs(x) * 0.4) ** 2) ** (-1), '-', color=green)
+    plt.plot(0, 0, 'o', color=orange)
+    jesseaxis(ax, x, x)
+    ax.axis('off')
+    plt.ylim(-0.2, 1.2)
+    saveimg('inversequadratic')
+    # plt.show()
+
+
+def makeInverseMultiQuadric():
+    plt.close()
+    x = np.linspace(-4, 4, 1000)
+    ax = plt.axes()
+    plt.plot(x, np.sqrt(1 + (np.abs(x) * 0.4) ** 2) ** (-1), '-', color=green)
+    plt.plot(0, 0, 'o', color=orange)
+    jesseaxis(ax, x, x)
+    ax.axis('off')
+    plt.ylim(-0.2, 1.2)
+    saveimg('inversemultiquadric')
+    # plt.show()
+
+def makekernels():
+    makeGaussian()
+    makeMultiQuadric()
+    makeInverseQuadratic()
+    makeInverseMultiQuadric()
+
+makekernels()
