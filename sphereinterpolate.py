28660427826fd7e3c0647315ef43c14e3292b383
Jesse <jessebett@gmail.com>
2014-08-06 14:15:28 -0400

    Implementing Mayavi instead of Matplotlib to correct plotting issues

--- sphereinterpolate.py
+++ sphereinterpolate.py
@@ -3,7 +3,9 @@ from matplotlib import cm, colors
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from scipy import special
+from scipy.interpolate import Rbf
 from collections import namedtuple
+from mayavi import mlab
 
 # Nice aliases
 pi = np.pi
@@ -32,13 +34,55 @@ def colorfunction(m, n, theta, phi):
     return special.sph_harm(m, n, theta, phi).real
 norm = colors.Normalize()
 
-fun = coordinates(3, 100j)
-interp = coordinates(3, 100j)
+fun = coordinates(1, 100j)
+interp = coordinates(1, 5j)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(fun.x, fun.y, fun.z)
-ax.plot_surface(
-    fun.x, fun.y, fun.z,  rstride=1, cstride=1, facecolors=cm.jet(norm(colorfunction(1, 3, fun.theta, fun.phi))))
-fig.savefig('sphere.png')
-plt.show()
+def sphere_norm(x,y):
+    return np.arccos(np.dot(x,y))
+
+harmonic=colorfunction(1, 3, fun.theta, fun.phi)
+
+rbf=Rbf(interp.phi, interp.theta, colorfunction(1, 3, interp.theta, interp.phi))
+interp_values=rbf(fun.phi,fun.theta)
+
+
+# rbf=Rbf(interp.x, interp.y, interp.z, colorfunction(1, 3, interp.theta, interp.phi))
+# interp_values=rbf(fun.x,fun.y,fun.z)
+
+mlab.figure()
+mlab.mesh(fun.x, fun.y, fun.z,scalars=harmonic)
+mlab.points3d(interp.x, interp.y, interp.z,scale_factor=0.1)
+mlab.show()
+
+
+
+# fig = plt.figure(figsize=(13,5))
+
+# #Function Figure
+# ax1 = fig.add_subplot(121, projection='3d')
+# ax1.scatter(
+#     interp.x, interp.y, interp.z, 
+#     color='r', marker='o',
+#     zorder=1, 
+#     s=40
+#     )
+
+# ax1.plot_surface(
+#     fun.x, fun.y, fun.z,  
+#     rstride=1, 
+#     cstride=1, 
+#     alpha=0.5,
+#     facecolors=cm.jet(norm(harmonic)),
+#     zorder=-1
+#     )
+
+# ax2 = fig.add_subplot(122, projection='3d')
+# ax2.plot_surface(
+#     fun.x, fun.y, fun.z,  
+#     rstride=1, 
+#     cstride=1, 
+#     alpha=1,
+#     facecolors=cm.jet(norm(interp_values))
+#     )
+# fig.savefig('sphere.png')
+# plt.show()
