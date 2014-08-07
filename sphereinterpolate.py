3c3ff82e607986b34df1e4cba8dcdb5147c5822d
Jesse <jessebett@gmail.com>
2014-08-07 11:57:25 -0400

    Added figure for error comparison

--- sphereinterpolate.py
+++ sphereinterpolate.py
@@ -29,7 +29,10 @@ def coordinates(r, n):
 # fine is coordinates on a fine grid
 # interp is coordinates on coarse grid for training interpolation
 fine = coordinates(1, 100j)
-interp = coordinates(1, 5j)
+interp = coordinates(1, 6j)
+
+print interp.phi
+print interp.theta
 
 
 # Defining finection to colour sphere
@@ -43,13 +46,15 @@ harmonic13_fine = harmonic(1, 3, fine.theta, fine.phi)
 harmonic13_coarse = harmonic(1, 3, interp.theta, interp.phi)
 
 
-# Train the interpolation using coarse coordinates
+# Train the interpolation using interp coordinates
 rbf = Rbf(interp.phi, interp.theta, harmonic13_coarse)
 # The result of the interpolation on fine coordinates
 interp_values = rbf(fine.phi, fine.theta)
 
+error=harmonic13_fine-interp_values
+L_infinity=np.max(np.abs(error))
 
-# rbf=Rbf(interp.x, interp.y, interp.z, harmonic(1, 3, interp.theta, interp.phi))
+# rbf=Rbf(interp.x, interp.y, interp.z, harmonic13_coarse)
 # interp_values=rbf(fine.x,fine.y,fine.z)
 
 #Figure of harmoinc function on sphere in fine cordinates
@@ -59,13 +64,19 @@ vmax, vmin = np.max(harmonic13_fine), np.min(harmonic13_fine)
 mlab.mesh(fine.x, fine.y, fine.z, scalars=harmonic13_fine, vmax=vmax, vmin=vmin)
 mlab.points3d(interp.x, interp.y, interp.z, harmonic13_coarse,
               scale_factor=0.1, scale_mode='none', vmax=vmax, vmin=vmin)
+# mlab.savefig('interppointssphere.png')
 
 #Figure showing results of rbf interpolation
 mlab.figure()
-vmax, vmin = np.max(harmonic13_fine), np.min(harmonic13_fine)
-mlab.mesh(fine.x, fine.y, fine.z, scalars=interp_values)
-# mlab.points3d(interp.x, interp.y, interp.z, scalars, scale_factor=0.1, scale_mode='none',vmax=vmax, vmin=vmin)
+mlab.mesh(fine.x, fine.y, fine.z, scalars=interp_values, vmax=vmax,vmin=vmin)
+mlab.points3d(interp.x, interp.y, interp.z, harmonic13_coarse, scale_factor=0.1, scale_mode='none',vmax=vmax, vmin=vmin)
+# mlab.savefig('interpsphere.png')
 
+mlab.figure()
+mlab.mesh(fine.x, fine.y, fine.z, scalars=error, vmax=L_infinity,vmin=-L_infinity)
+mlab.colorbar(title='Error', orientation='vertical')
+# mlab.points3d(interp.x, interp.y, interp.z, scalars, scale_factor=0.1, scale_mode='none',vmax=vmax, vmin=vmin)
+# mlab.savefig('interpsphere.png')
 
 mlab.show()
 
